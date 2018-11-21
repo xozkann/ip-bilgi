fbcf8ac420bed07e188ade8a85df503fc67e107c
Buğra Özkan <38759414+xozkann@users.noreply.github.com>
2018-11-21 21:29:27 +0300

    Update index.php
    IP bilgi ve konum öğrenme.

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
             echo '<hr>';
             echo '<b>Harita:</b>';
             echo '<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script><div style="overflow:hidden;height:440px;width:700px;"><div id="gmap_canvas" style="height:440px;width:700px;"></div><div><small><a href="embed google map">http://embedgooglemaps.com</a></small></div><div><small><a href="https://googlemapsgenerator.com">embed google maps</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type="text/javascript">function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(39.9333635,32.85974190000002),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng('.$json_coz['lat'].','.$json_coz['lon'].')});infowindow = new google.maps.InfoWindow({content:"<strong>'.$json_coz['query'].'</strong><br>'.$json_coz['city'].', '.$json_coz['country'].'<br>"});google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, "load", init_map);</script> ';
-            echo '<br>* Harita (Şehir ve Ülke) dışında ki yerleri doğru göstermeyebilir.';
+            echo '<br><b>*</b> Harita (Şehir ve Ülke) dışında ki yerleri doğru göstermeyebilir.';
         }
         ?>
     </body>
